27fd0abfff699a4086be7083548e01337791eaf5
marzke <ron.marzke@gmail.com>
2020-01-11 14:55:15 -0800

    Initial mod

--- app.py
+++ app.py
@@ -7,10 +7,10 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div(children=[
-    html.H1(children='Hello World'),
+    html.H1(children='Enrollment Dashboard v0.1'),
 
     html.Div(children='''
-        Dash: A web application framework for Python.
+        Questions? Contact marzke@sfsu.edu.
     '''),
 
     dcc.Graph(
